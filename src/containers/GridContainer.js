2eaed00bca42371e520a638f836ad3752015f80a
sean-slaughter <sthompson2822@gmail.com>
2020-10-12 10:00:57 -0500

    Add GridContainer

---
+++ src/containers/GridContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class GridContainer extends Component {
+    render() {
+        return (
+            <div>
+                Grid Container
+            </div>
+        )
+    }
+}
+
+export default GridContainer
+
