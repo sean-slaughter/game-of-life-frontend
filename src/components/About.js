bae3037368900a31567388a9af20fc04d1e17b2b
sean-slaughter <sthompson2822@gmail.com>
2020-10-16 20:22:23 -0500

    Add skeleton of About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,17 +3,46 @@ import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
 
 const D
 
 
 export class About extends Component {
+    
+    state = {
+        open: false
+    }
+
+    handleClose(){
+        this.setState({
+            open: !this.state.open
+        })
+    }
 
     render() {
+        const { open } = this.state
+
         return (
-            <div>
-                
-            </div>
+            <Dialog onClose={this.handleClose} open={open}>
+                <DialogTitle id="simple-dialog-title">
+                    About Conways Game of Life
+                    <DialogContent>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Laudantium ea vel excepturi, atque iusto reiciendis soluta alias. 
+                        Nemo obcaecati eos ullam beatae, nihil, earum totam numquam autem excepturi, 
+                        alias inventore?
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sequi aliquam sapiente quia, incidunt consequatur dolor a 
+                        consectetur voluptates? Sapiente architecto dolorum molestiae vel 
+                        commodi provident veniam, esse autem eligendi velit.
+                    </DialogContent>
+                </DialogTitle>
+
+            </Dialog>
+           
         )
     }
 }
