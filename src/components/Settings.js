54f51282c0ee8bec55752ad3afb8987a9ad42a9a
sean-slaughter <sthompson2822@gmail.com>
2020-10-17 18:39:20 -0500

    Add settings component

---
+++ src/components/Settings.js
@@ -0,0 +1,26 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+function Settings(props) {
+    return (
+      <>
+        
+      </>
+    )
+}
+
+const mapStateToProps = state =>{
+    return{
+
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings)
