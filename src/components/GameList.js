274e8f27ab1c592c2f071381109c875c874e562f
sean-slaughter <sthompson2822@gmail.com>
2020-10-18 12:49:35 -0500

    Create gamelist component

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,12 +1,34 @@
+import { Dialog, DialogContent, DialogTitle } from '@material-ui/core';
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { connect } from 'react/redux';
+import { fetchGames } from '../actions/gameActions'
 
-function GameList() {
+
+function GameList(props) {
+
+    useEffect(props.fetchGames,[])
+
     return (
         <div>
-            
+            <Dialog>
+                <DialogTitle>
+                    List of Saved Games
+                </DialogTitle>
+                <DialogContent>
+                    Games
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
 
-export default connect()(GameList)
+const mapStateToProps = state => {
+    return {
+        games: state.games
+    }
+}
+
+
+export default connect(mapStateToProps, { fetchGames })(GameList)
