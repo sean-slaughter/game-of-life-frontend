ea80338a2d78741b5f604fbe867e00a06cd239df
sean-slaughter <sthompson2822@gmail.com>
2020-10-17 19:21:29 -0500

    Add save settings button

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,4 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
@@ -8,32 +8,47 @@ import { connect } from 'react-redux'
 function Settings(props) {
 
     const [open, setOpen] = useState(false)
-    const {rows, cols, cellSize, speed} = props.settings;
+    const [settings, setSettings] = useState(props.settings)
 
     useEffect(() => {
         setOpen(true)
     },[])
 
+
+    const handleClose = () => {
+        setOpen(false);
+        props.history.push('/home')
+      };
+
+    const saveSettings = () => {
+        
+    }
+
     return (
-      <>
-        <Dialog>
+         <Dialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
             <DialogTitle>
-                Title
+                Edit the settings of your game grid:
             </DialogTitle>
-            <DialogContent>
+            <DialogContent dividers>
                 Content
             </DialogContent>
             <DialogActions>
-                Actions
+                <Button color="primary" onClick={saveSettings}>Save Settings</Button> 
             </DialogActions>
         </Dialog>
-      </>
     )
 }
 
 const mapStateToProps = state =>{
     return{
-
+        rows: state.settings.rows,
+        cols: state.settings.cols,
+        speed: state.settings.speed,
+        cellSize: state.settings.cellSize
     }
 }
 
