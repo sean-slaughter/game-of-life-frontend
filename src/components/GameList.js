c2dc6ea19bb9281ce3d85eec2bcb1a8e0d12e377
sean-slaughter <sthompson2822@gmail.com>
2020-10-18 10:42:19 -0500

    Add game list component

---
+++ src/components/GameList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { connect } from 'react/redux';
+
+function GameList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default connect()(GameList)
