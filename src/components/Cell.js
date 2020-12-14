6d9ef84bc4074da1cd392e41eef25c930fb22e8f
sean-slaughter <sthompson2822@gmail.com>
2020-12-14 11:37:53 -0600

    Remove unneccessary comments

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -17,23 +17,6 @@ function Cell(props) {
   );
 }
 
-// export class Cell extends Component {
-//   handleClick = () => {
-//     console.log(this.props.row, this.props.col);
-//     this.props.switchCell(this.props.row, this.props.col);
-//   };
-
-//   render() {
-//     return (
-//       <div
-//         style={{ width: this.props.cellSize, height: this.props.cellSize }}
-//         className={this.props.cellState}
-//         onClick={this.handleClick}
-//       ></div>
-//     );
-//   }
-// }
-
 const mapDispatchToProps = (dispatch) => {
   return {
     switchCell: (row, col) => {
